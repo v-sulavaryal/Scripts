8f7a4cd6ab3fc594df064c49cd427c7689f034ac
v-sulavaryal <82856842+v-sulavaryal@users.noreply.github.com>
2021-05-24 18:53:52 -0700

    Rehydrate blobs by changing tier

---
+++ Rehydrate.ps1
@@ -0,0 +1,25 @@
+﻿Connect-AzAccount
+Set-AzContext -SubscriptionId "your-subscription-id"
+$resourceGroup = "your-resource-group-name"
+$storageAccountName = "your-storage-account-name"
+$containerName = "your-container-name"
+$directoryName = "your-directory-name"
+
+
+# get a reference to the storage account and the context
+$storageAccount = Get-AzStorageAccount `
+  -ResourceGroupName $resourceGroup `
+  -Name $storageAccountName
+$ctx = $storageAccount.Context 
+
+# get a list of all of the blobs in the directory filtered by 
+$listOfBlobs = Get-AzStorageBlob -Prefix $directoryName -Container $containerName -Context $ctx | Where-Object{$_.ICloudBlob.Properties.StandardBlobTier -eq "Archive"}
+
+Write-Host $listOfBlobs.Count
+
+# this loops through the list of blobs and changes blog tiers. 
+foreach($blob in $listOfBlobs){
+    $blob.ICloudBlob.SetStandardBlobTier("Hot", "Standard");
+}
+
+Disconnect-AzAccount
